c218f90595e8c684589e02f4938cde44c16a3ac4
Sujoy Gupta <sujoy@pingbooth.com>
2012-10-16 16:41:39 -0700

    Add better messaging for tests and allow filters to be specified as a string or as a symbol.

--- lib/specstar/controllers.rb
+++ lib/specstar/controllers.rb
@@ -12,7 +12,7 @@ module Specstar
       def has_skip_before_filter?(controller, filter, action)
         controller._process_action_callbacks.select { |callback|
           callback.chain.select { |chain|
-            chain.kind == :before && chain.filter == filter && (action.nil? || chain.per_key[:unless].include?("action_name == '#{action}'"))
+            chain.kind == :before && chain.filter.to_s == filter.to_s && (action.nil? || chain.per_key[:unless].include?("action_name == '#{action}'"))
           }.size > 0
         }.size > 0
       end
@@ -21,7 +21,7 @@ module Specstar
         callbacks = controller.is_a?(ApplicationController) ? controller._process_action_callbacks : controller._dispatch_callbacks
         callbacks.select { |callback|
           callback.chain.select { |chain|
-            chain.kind == :before && chain.filter == filter && (action.nil? || chain.per_key[:if].include?("action_name == '#{action}'"))
+            chain.kind == :before && chain.filter.to_s == filter.to_s && (action.nil? || chain.per_key[:if].include?("action_name == '#{action}'"))
           }.size > 0
         }.size > 0
       end
@@ -34,6 +34,18 @@ module Specstar
         match do |controller|
           has_skip_before_filter?(controller, filter, @action)
         end
+
+    	failure_message_for_should do |controller|
+      	  "Expected #{controller.class.name} to have a skip before filter '#{filter}'."
+    	end
+
+    	failure_message_for_should_not do |controller|
+      	  "Expected #{controller.class.name} not to have a skip before filter '#{filter}'."
+    	end
+
+    	description do
+      	  "have a skip before filter '#{filter}'."
+    	end
       end
 
       RSpec::Matchers.define :have_before_filter do |filter|
@@ -44,6 +56,18 @@ module Specstar
         match do |controller|
           has_before_filter?(controller, filter, @action)
         end
+
+    	failure_message_for_should do |controller|
+      	  "Expected #{controller.class.name} to have a before filter '#{filter}'."
+    	end
+
+    	failure_message_for_should_not do |controller|
+      	  "Expected #{controller.class.name} not to have a before filter '#{filter}'."
+    	end
+
+    	description do
+      	  "have a before filter '#{filter}'."
+    	end
       end
     end
   end
